170fdd37392ef2e7a0d8d7ccb1fa5af82761b6d5
Chen Li <lchen139@gmail.com>
2018-08-10 18:06:20 -0500

    Create lengthOfLongestSubstring.java

---
+++ lengthOfLongestSubstring.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int result = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+        	if(map.containsKey(s.charAt(i))) {
+        		start = Math.max(start, map.get(s.charAt(i)) + 1);
+        	}
+        	map.put(s.charAt(i), i);
+        	result = Math.max(result, i - start + 1);
+        }
+        return result;
+    }
+}
